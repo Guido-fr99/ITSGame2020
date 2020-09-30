70bf6d6d76f30e1a423bd4b674193c3404f48c5a
Guido Franceschelli <allievo_franceschelli.g@itsumbria.it>
2020-09-30 12:54:15 +0200

    Commit modif

--- ITSUmbriaGame/Game1/Program.cs
+++ ITSUmbriaGame/Game1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Reflection.PortableExecutable;
+using ITSUmbria2020.OnlineGame.Library.Models.Characters;
 
 namespace Game1
 {
@@ -6,7 +8,14 @@ namespace Game1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Character character = new Wizard();
+            PrintCharacter(new Wizard());
+        }
+        private static void PrintCharacter(Character character)
+        {
+            Console.ForegroundColor = (ConsoleColor.Green);
+            Console.WriteLine(character.Name);
+            Console.ResetColor();
         }
     }
 }
---
+++ ITSUmbriaGame/ITSUmbria2020.OnlineGame.Library.Models/Characters/Character.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ITSUmbria2020.OnlineGame.Library.Models.Characters
+{
+    public abstract class Character
+    {
+        public abstract int MaxHealth { get; }
+
+        public string Name { get; set; } = "Player";
+        public Gender Gender { get; set; }
+        public CharacterType Type { get; set; }
+        
+    }
+    public class Wizard : Character
+    {
+        public override int MaxHealth => 110;
+    }
+    public class Rogue : Character
+    {
+        public override int MaxHealth => 150;
+    }
+    public class Fighter : Character
+    {
+        public override int MaxHealth => 200;
+    }
+    public enum Gender
+    {
+        Unknown,
+        Male,
+        Female
+    }
+    public enum CharacterType
+    {
+        Human,
+        Elf,
+        Orc
+
+    }
+}
